825c0f83daad4a172762e4e2783df50b0eaa7799
cjbrock <corinna@flatironschool.com>
2020-01-28 08:32:02 -0500

    add all the files

---
+++ src/index.js
@@ -0,0 +1,10 @@
+fetch('http://localhost:3000/hogs')
+    .then(resp => resp.json())
+    .then(hogs => {
+        hogs.forEach(hog=> {
+            const { id, name, specialty, greased, image } = hog
+            const weight = hog['weight as a ratio of hog to LG - 24.7 Cu. Ft. French Door Refrigerator with Thru-the-Door Ice and Water']
+            const medal = hog['highest medal achieved']
+            new Hog(id, name, specialty, greased, weight, medal, image)
+        })
+    })
